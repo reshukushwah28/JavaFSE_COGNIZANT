8c241907a41d0569f7bbcf03c2b0e4c45d01b085
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-07-22 00:44:03 +0530

    Add Week 7: Gateway Load Balancer

---
+++ Week_7/gateway-loadbalancer/src/test/java/com/example/gateway_loadbalancer/GatewayLoadbalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gateway_loadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayLoadbalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
