5ab7a8bea520540664c523d3df465cba7196de27
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-07-22 00:22:40 +0530

    Add Week 7: OAuth2 Client centralized authentication

---
+++ Week_7/oauth2-client/src/main/java/com/example/oauth2_client/UserController.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_client;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
+
+@RestController
+public class UserController {
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+}
