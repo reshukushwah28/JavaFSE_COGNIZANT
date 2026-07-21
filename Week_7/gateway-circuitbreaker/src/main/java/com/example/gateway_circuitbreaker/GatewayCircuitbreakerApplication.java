7baa58a9e94b0ba72b5442ab40882ff83862a347
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-07-22 00:44:04 +0530

    Add Week 7: Gateway Circuit Breaker

---
+++ Week_7/gateway-circuitbreaker/src/main/java/com/example/gateway_circuitbreaker/GatewayCircuitbreakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gateway_circuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayCircuitbreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayCircuitbreakerApplication.class, args);
+	}
+
+}
---
+++ Week_7/gateway-circuitbreaker/src/test/java/com/example/gateway_circuitbreaker/GatewayCircuitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gateway_circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayCircuitbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
